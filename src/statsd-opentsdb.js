6a547d5627e0a648fe61c47a617524cbe828c07d
Filip Stollar <filip.stollar@skyscanner.net>
2017-07-28 16:52:14 +0200

    Further progress with configurable tags

--- src/statsd-opentsdb.js
+++ src/statsd-opentsdb.js
@@ -15,6 +15,8 @@
 var net = require('net'),
     util = require('util');
     request = require('request');
+    os = require("os");
+
 
 var debug;
 var flushInterval;
@@ -28,55 +30,48 @@ var prefixTimer;
 var prefixGauge;
 var prefixSet;
 
+var tagSet;
+
 var opentsdbStats = {};
 
 var post_stats = function opentsdb_post_stats(statString) {
-  var last_flush = opentsdbStats.last_flush || 0;
-  var last_exception = opentsdbStats.last_exception || 0;
   if (opentsdbHost) {
     try {
       var ts = Math.round(new Date().getTime() / 1000);
-      statString += opentsdbPrefix + '.opentsdbStats.last_exception ' + last_exception + ' ' + ts + "\n";
-      statString += opentsdbPrefix + '.opentsdbStats.last_flush ' + last_flush + ' ' + ts + "\n";
       var metrics = statString.split("\n");
       for (var i = 0; i < metrics.length; i++) {
-           console.log(metrics[i]);
-           var metric = metrics[i].split(" ");
-           if (metric[1] && metric[2]) {
-             request.post(
-               opentsdbHost,
-                { json: { metric: metric[0],
-                        timestamp: metric[1],
-                        value: metric[2],
-                        tags: {hostname: "MYMACHINE"} } },
-                function (error, response, body) {
-                }
-             );
-           }
+        console.log(metrics[i]);
+        var metric = metrics[i].split(" ");
+        if (metric[1] && metric[2]) {
+          request.post(opentsdbHost,
+            { json: { metric: metric[0],
+                      timestamp: metric[1],
+                      value: metric[2],
+                      tags: {hostname: os.hostname()} } },
+              function (error, response, body) {
+                util.log(error);
+              }
+          );
+        }
       }
-      opentsdbStats.last_flush = Math.round(new Date().getTime() / 1000);
-    } catch(e){
+    } catch(e) {
       if (debug) {
         util.log(e);
       }
-      opentsdbStats.last_exception = Math.round(new Date().getTime() / 1000);
     }
   }
 }
 
 // Returns a list of "tagname=tagvalue" strings from the given metric name.
 function parse_tags(metric_name) {
-  var parts = metric_name.split(".");
+  var parts = metric_name.split('.');
   var tags = [];
-  var current_tag_name = "";
+  var current_tag_name = '';
   for (i in parts) {
-    var p = parts[i]
-    if (p.indexOf(opentsdbPrefix) == 0) {
-      var tag_name = p.split(opentsdbPrefix)[1];
-      current_tag_name = tag_name
-    } else if (current_tag_name != "") {
-      tags.push(current_tag_name + "=" + p);
-      current_tag_name = "";
+    for (var tag in tagSet) {
+      if (i === tag) {
+        console.log(tagSet[tag]);
+      }
     }
   }
 
@@ -85,7 +80,7 @@ function parse_tags(metric_name) {
 
 // Strips out all tag information from the given metric name
 function strip_tags(metric_name) {
-  var parts = metric_name.split(".");
+  var parts = metric_name.split('.');
   var rslt_parts = [];
   while (parts.length > 0) {
     if (parts[0].indexOf(opentsdbPrefix) == 0) {
@@ -96,7 +91,7 @@ function strip_tags(metric_name) {
     rslt_parts.push(parts.shift());
   }
 
-  return rslt_parts.join(".");
+  return rslt_parts.join('.');
 }
 
 
@@ -117,7 +112,7 @@ var flush_stats = function opentsdb_flush(ts, metrics) {
     var tags = parse_tags(key);
     var stripped_key = strip_tags(key)
     var value = counters[key];
-    statString += opentsdbPrefix + "." + prefixCounter + "." + stripped_key + ".count" + ' ' + ts + ' ' + value + ' ' + tags.join(' ') + suffix;
+    statString += opentsdbPrefix + '.' + prefixCounter + '.' + stripped_key + '.count' + ' ' + ts + ' ' + value + ' ' + tags.join(' ') + suffix;
   }
 
   for (key in timer_data) {
@@ -125,7 +120,7 @@ var flush_stats = function opentsdb_flush(ts, metrics) {
       for (timer_data_key in timer_data[key]) {
         var tags = parse_tags(key);
         var stripped_key = strip_tags(key)
-        var the_key = opentsdbPrefix + "." + prefixTimer + "." + stripped_key;
+        var the_key = opentsdbPrefix + '.' + prefixTimer + '.' + stripped_key;
         statString += the_key + '.' + timer_data_key + ' ' + ts + ' ' + timer_data[key][timer_data_key] + ' ' + tags.join(' ') + suffix;
       }
     }
@@ -134,19 +129,18 @@ var flush_stats = function opentsdb_flush(ts, metrics) {
   for (key in gauges) {
     var tags = parse_tags(key);
     var stripped_key = strip_tags(key)
-    statString += opentsdbPrefix + '.' + prefixGauge + '.gauge ' + ts + ' ' + gauges[key] + ' ' + tags.join(' ') + suffix;
+    statString += opentsdbPrefix + '.' + prefixGauge + '.' + stripped_key + '.gauge ' + ts + ' ' + gauges[key] + ' ' + tags.join(' ') + suffix;
   }
 
   for (key in sets) {
-    var tags = parse_tags(key);
     var stripped_key = strip_tags(key)
-    statString += opentsdbPrefix + '.' + prefixSet + '.count ' + ts + ' ' + sets[key].values().length + ' ' + tags.join(' ') + suffix;
+    var tags = parse_tags(key);
+    var name = opentsdbPrefix + '.' + prefixSet + '.' + stripped_key
+    statString += name + '.count ' + ts + ' ' + sets[key].values().length + ' ' + tags.join(' ') + suffix;
   }
 
-  statString += opentsdbPrefix + '.opentsdbStats.calculationtime ' + ts + ' ' + (Date.now() - starttime) + suffix;
   for (key in statsd_metrics) {
-    var the_key = opentsdbPrefix + "." + key;
-    statString += the_key + ' ' + ts + ' ' + statsd_metrics[key];
+    statString += opentsdbPrefix + '.' + key + ' ' + ts + ' ' + statsd_metrics[key];
   }
 
   post_stats(statString);
@@ -171,14 +165,12 @@ exports.init = function opentsdb_init(startup_time, config, events) {
   prefixTimer       = config.opentsdb.prefixTimer;
   prefixGauge       = config.opentsdb.prefixGauge;
   prefixSet         = config.opentsdb.prefixSet;
+  tagSet            = config.opentsdb.tags;
 
-  prefixCounter = prefixCounter !== undefined ? prefixCounter : "";
-  prefixTimer   = prefixTimer !== undefined ? prefixTimer : "";
-  prefixGauge   = prefixGauge !== undefined ? prefixGauge : "";
-  prefixSet     = prefixSet !== undefined ? prefixSet : "";
-
-  opentsdbStats.last_flush = startup_time;
-  opentsdbStats.last_exception = startup_time;
+  prefixCounter = prefixCounter !== undefined ? prefixCounter : '';
+  prefixTimer   = prefixTimer !== undefined ? prefixTimer : '';
+  prefixGauge   = prefixGauge !== undefined ? prefixGauge : '';
+  prefixSet     = prefixSet !== undefined ? prefixSet : '';
 
   flushInterval = config.flushInterval;
 
